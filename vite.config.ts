ad2ff145f4ca7244ff75193858e9441e17fa8026
malkiAbdoo <abdo.malkiep@gmail.com>
2024-08-27 23:03:34 +0100

    fix: add the base-path on production

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
+import pkg from './package.json';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? `/${pkg.name}` : '/',
   plugins: [react()],
 });
